71b00b34025daed63270443d8026746e31f86ec8
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2017-04-14 15:56:55 +0530

    updating

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ module.exports.ctx = function (req, res, next) {
     req.serand = req.serand || {};
     next();
 };
+
+module.exports.pond = function (req, res, next) {
+    res.pond = function (o) {
+        res.status(o.status).send(o.data);
+    };
+    next();
+};
+
+module.exports.locate = function (prefix) {
+    return function (req, res, next) {
+        res.locate = function (path) {
+            return res.location(req.protocol + '://' + req.get('host') + prefix + path);
+        };
+        next();
+    };
+};
