170f852cf5aeb966c0bb3bd8c6833eaeb0f53f3a
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2017-07-14 20:53:50 +0530

    updating

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+var log = require('logger')('serandi:index');
+var _ = require('lodash');
+var nconf = require('nconf');
+var url = require('url');
+
+var port = nconf.get('port');
+
 module.exports.ctx = function (req, res, next) {
     req.serand = req.serand || {};
     next();
@@ -17,4 +24,67 @@ module.exports.locate = function (prefix) {
         };
         next();
     };
+};
+
+module.exports.many = function (req, res, next) {
+    res.many = function (o) {
+        var data = req.query.data;
+        var paging = data.paging;
+        if (o.length < paging.count) {
+            return res.send(o);
+        }
+        var sort = paging.sort;
+        var end = o[o.length - 1];
+        if (!end) {
+            return res.send(o);
+        }
+        var pathname = req.baseUrl;
+        if (req.path !== '/') {
+            pathname += req.path;
+        }
+        if (sort._id) {
+            sort.id = sort._id;
+            delete sort._id;
+        }
+        var link = function (rel, direction) {
+            if (!rel) {
+                return null;
+            }
+            var order;
+            var fields = {};
+            Object.keys(sort).forEach(function (field) {
+                fields[field] = rel[field];
+                if (order) {
+                    return;
+                }
+                order = sort[field];
+            });
+            if (!order) {
+                return null;
+            }
+            var cursor = {};
+            var type = (direction * order === 1) ? 'min' : 'max';
+            cursor[type] = fields;
+            var clone = _.cloneDeep(data);
+            clone.paging.cursor = cursor;
+            return url.format({
+                protocol: req.protocol,
+                hostname: req.hostname,
+                port: nconf.get('port'),
+                pathname: pathname,
+                query: {
+                    data: JSON.stringify(clone)
+                }
+            });
+        };
+        var index = o.length - 1;
+        var last = link(o[0], -1);
+        var next = link(o[index], 1);
+        res.links({
+            last: last,
+            next: next
+        });
+        res.send(o);
+    };
+    next();
 };
